0a84e8f63d1610f396ee6d0567de59f96e50b20a
sangeeta-chaukulkar <91315460+sangeeta-chaukulkar@users.noreply.github.com>
2022-04-21 16:22:46 +0530

    added order functionality

--- routes/shop.js
+++ routes/shop.js
@@ -14,12 +14,13 @@ router.get('/products/:productId', shopController.getProduct);
 
 router.get('/cart', shopController.getCart);
 
+router.get('/carts', shopController.getCards);
+
 router.post('/cart', shopController.postCart);
 
 router.post('/cart-delete-item', shopController.postCartDeleteProduct);
 
-router.get('/orders', shopController.getOrders);
+router.post('/create-order', shopController.postOrder);
 
-router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
